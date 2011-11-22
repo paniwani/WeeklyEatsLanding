e42d46d29f732ce12437783260acc74c8d3aad4a
Neil Panjwani <paniwani@gmail.com>
2011-11-21 23:46:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeeklyEatsLanding::Application.config.secret_token = '4d7d266668ca508ccc80d02f99fbc6c06bc6ac53cb9bb2032840c87adbbdb9775aa9f41a807c7e74835034b9fb63e8c1fef1c0d453683c64c159c924c794401f'
